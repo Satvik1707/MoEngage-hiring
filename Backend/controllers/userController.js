e0b1259c649f78b246e5acffdcdd1cde32c7fd8c
Satvik Madaan <satvik0125@gmail.com>
2023-11-05 02:01:27 +0530

    server cleanup

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { secretKey } = require('../config/config');
+
+exports.register = async (req, res) => {
+    try {
+        const { email, username, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({ email, username, password: hashedPassword })
+        await newUser.save()
+        res.status(201).json({ message: 'User created successfully' })
+    } catch (error) {
+        res.status(500).json({ error: 'Error signing up' })
+    }
+};
+
+
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials'})
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid credentials' })
+        }
+        const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '1hr' })
+        res.json({ message: 'Login successful' })
+    } catch (error) {
+        res.status(500).json({ error: 'Error logging in' })
+    }
+};
